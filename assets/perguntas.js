a237624cc94d46e6fda2db741188a008888725ef
karolhonorio <karoline.honorio.souza@escola.pr.gov.br>
2025-06-05 12:22:41 -0300

    Primeiro commit
    Começando o meu projeto.

---
+++ assets/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Esporte',
+    'Quantas copas do mundo o Brasil tem?',
+    'A seleção brasileira tem 5 copas do mundo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior país da América do Sul?',
+    'O Brasil é o maior país da América do Sul'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o país mais populoso do mundo?',
+    'A Índia é o país mais populoso do mundo'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o animal conhecido por ser mamífero e botar ovo?',
+    'Ornitorrinco'
+)
+
+criaCartao(
+    'Português',
+    'Como se conjulga o verbo por?',
+    'Ponho, pões, põe, pomos, pondes, põem.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a unidade básica da vida?',
+    'A célula é a unidade básica da vida'
+)
+
+criaCartao(
+    'História',
+    'Em que ano foi assinada a Lei Áurea?',
+    'No ano 1888'
+)
+
+criaCartao(
+    'Arte',
+    'Quais são os 4 tipos de arte',
+    'Artes visuais, música, teatro e dança'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
